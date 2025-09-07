a521869211a209eb3f63e1ae0e506d8d6bb2cafe
Panchikhin Maksim <panchihin@list.ru>
2025-09-07 18:58:03 +0300

    удалили закоменченые строки

--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -8,7 +8,6 @@ class TimeStampedMixin(models.Model):
     modified = models.DateTimeField(auto_now=True)
 
     class Meta:
-        # Этот параметр указывает Django, что этот класс не является представлением таблицы
         abstract = True
 
 class UUIDMixin(models.Model):
@@ -18,18 +17,14 @@ class UUIDMixin(models.Model):
         abstract = True
 
 class Genre(UUIDMixin, TimeStampedMixin):
-    # Первым аргументом обычно идёт человекочитаемое название поля
     name = models.CharField(_('name'), max_length=255)
-    # blank=True делает поле необязательным для заполнения.
     description = models.TextField(_('description'), blank=True)
 
     def __str__(self):
         return self.name 
 
     class Meta:
-        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
         db_table = "content\".\"genre"
-        # Следующие два поля отвечают за название модели в интерфейсеca
         verbose_name = 'Жанр'
         verbose_name_plural = 'Жанры'
 
@@ -68,7 +63,6 @@ class TypeChoices(models.TextChoices):
     TV_SHOW = 'tv_show', 'TV Show'
 
 class FilmWork(UUIDMixin, TimeStampedMixin):
-    # certificate = models.CharField(_('certificate'), max_length=512, blank=True)
     title = models.CharField(_('title'), max_length=255, unique=True)
     description = models.TextField(_('description'), blank=True)
     creation_date = models.DateField()
